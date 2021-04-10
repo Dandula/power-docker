675a26fea7cc233a6d74f4c5e433af85cc041ab1
Vladyslav Revenko <dandular@gmail.com>
2021-04-10 23:14:43 +0300

    feat(packages): added ability to use cache by package managers

--- tools/composer.sh
+++ tools/composer.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-mkdir -p .composer
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+WORKSPACE_DIR="${SCRIPT_DIR%/*}"
+COMPOSER_DIR="${WORKSPACE_DIR}/data/cache/.composer"
 
-docker run --rm -it \
-  -v "${PWD}:/app" \
-  -u "${UID}:$(id -g)" \
-  composer "$@" --ignore-platform-reqs
+if [ -n "$SSH_AUTH_SOCK" ]; then
+  docker run --rm -it \
+    -v "${PWD}:/app" \
+    -v "${COMPOSER_DIR}:/tmp" \
+    -v $SSH_AUTH_SOCK:/ssh-auth.sock \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -e SSH_AUTH_SOCK=/ssh-auth.sock \
+    -u "${UID}:$(id -g)" \
+    composer "$@" --ignore-platform-reqs
+else
+  docker run --rm -it \
+    -v "${PWD}:/app" \
+    -v "${COMPOSER_DIR}:/tmp" \
+    -u "${UID}:$(id -g)" \
+    composer "$@" --ignore-platform-reqs
+fi
--- tools/npm.sh
+++ tools/npm.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+WORKSPACE_DIR="${SCRIPT_DIR%/*}"
+NPM_DIR="${WORKSPACE_DIR}/data/cache/.npm"
+
 docker run --rm -it \
   -w "/usr/src/app" \
   -v "${PWD}:/usr/src/app" \
+  -v "${NPM_DIR}:/home/node/.npm" \
   -u "${UID}:$(id -g)" \
   node npm "$@"
